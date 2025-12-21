e709711b2ed2188823c81a584fb63471def19b43
Luka <kjh08116@naver.com>
2025-12-21 13:59:47 +0900

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// ✅ 지금 주소가 /portfolio/ 이므로 base도 동일하게 맞춤
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: "/portfolio-react/",
 });
